1c2b6ff57b571aaf06226994d8da1bc33bc97382
Bethan Palmer <bethan.palmer@digital.justice.gov.uk>
2020-10-01 12:16:12 +0100

    Remove spring security
    This app is not using spring security (it is using CXF secruity for credential checking)
    and having this dependency means the app fails to start up when we add the actuator dependency.
    
    As it's not being used we can remove it.

--- opa_10_12_transform/src/main/java/uk/gov/justice/laa/ccms/SpringCxfApplication.java
+++ opa_10_12_transform/src/main/java/uk/gov/justice/laa/ccms/SpringCxfApplication.java
@@ -2,12 +2,11 @@ package uk.gov.justice.laa.ccms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.WebApplicationInitializer;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 @ComponentScan(basePackages = {"com.oracle.determinations.server", "uk.gov.justice.laa.ccms"})
 public class SpringCxfApplication extends SpringBootServletInitializer implements
     WebApplicationInitializer {
