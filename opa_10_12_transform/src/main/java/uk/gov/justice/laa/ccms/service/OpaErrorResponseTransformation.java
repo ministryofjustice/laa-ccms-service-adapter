55e8fe392396e41910a25a7362a4efc75a590619
@murali23 <muralidhar.puttanna@legalaid.gsi.gov.uk>
2020-11-30 12:32:25 +0000

    Fixed SoapFault data transformation issue

--- opa_10_12_transform/src/main/java/uk/gov/justice/laa/ccms/service/OpaErrorResponseTransformation.java
+++ opa_10_12_transform/src/main/java/uk/gov/justice/laa/ccms/service/OpaErrorResponseTransformation.java
@@ -9,8 +9,7 @@ import com.oracle.determinations.server._10_0.rulebase.types.ObjectFactory;
 import com.oracle.determinations.server._10_0.rulebase.types.RulebaseEvent;
 import com.oracle.determinations.server._12_2.rulebase.assess.types.AttributeNodeType;
 import com.oracle.determinations.server._12_2.rulebase.assess.types.DecisionReportType;
-import java.util.Iterator;
-import java.util.NoSuchElementException;
+import java.util.List;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
@@ -18,8 +17,6 @@ import org.apache.cxf.binding.soap.SoapFault;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 import uk.gov.justice.laa.ccms.soap.error.Detail;
 import uk.gov.justice.laa.ccms.soap.error.Event;
 
@@ -29,58 +26,7 @@ public class OpaErrorResponseTransformation {
   private static final Logger logger = LoggerFactory.getLogger(OpaErrorResponseTransformation.class);
 
   private final String OPA_EVENT_ERROR = "assess.request.event.error";
-  /*
-  public AssessResponse tranformSoapFault (SoapFault soapFault) {
 
-    Element element = soapFault.getDetail();
-
-    if (  "detail".equalsIgnoreCase(element.getTagName()) ){
-
-      Node errorResponseNode = element.getFirstChild();
-
-      if ( errorResponseNode.hasChildNodes() ){
-
-        NodeList nodeList = errorResponseNode.getChildNodes();
-        for (Node node : iterable(nodeList)) {
-          if ( "code".equalsIgnoreCase(node.getLocalName()) ){
-            logger.debug("Code value : "+node.getTextContent());
-          } else if( "message".equalsIgnoreCase(node.getLocalName()) ){
-            logger.debug("Message value : "+node.getTextContent());
-          } else if( "events".equalsIgnoreCase(node.getLocalName()) ){
-            //Retrieve events data
-            Node eventNode = node.getFirstChild();
-            if ( "event".equalsIgnoreCase(eventNode.getLocalName()) ){
-              //<typ:event entity-id="BANKACC" instance-id="87491089" name="error">
-              NamedNodeMap map = eventNode.getAttributes();
-              String entityId = eventNode.getAttributes().getNamedItem("entity-id").getTextContent();
-              String instanceId = eventNode.getAttributes().getNamedItem("instance-id").getTextContent();
-              String name = eventNode.getAttributes().getNamedItem("name").getTextContent();
-              logger.debug("entityId : instanceId : name : " + entityId + " : " + instanceId + " : " + name);
-
-              NodeList eventChildNodeList = eventNode.getChildNodes();
-              for ( Node eventChildNode : iterable(eventChildNodeList) ){
-                if ( "message".equalsIgnoreCase(eventChildNode.getLocalName()) ){
-                  logger.debug("Error Message : "+eventChildNode.getTextContent());
-                } else if( "parameters".equalsIgnoreCase(eventChildNode.getLocalName()) ){
-                  logger.debug("Parameter Value " + eventChildNode.getFirstChild().getTextContent());
-                } else if ( "decision-report".equalsIgnoreCase(eventChildNode.getLocalName()) ){
-                  logger.debug("Decision Report " + eventChildNode.getLocalName());
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-    try {
-      getDetail(soapFault);
-    } catch (JAXBException e) {
-      logger.error("********************* ERROR ******************");
-      e.printStackTrace();
-    }
-    return null;
-  }
-  */
   public AssessResponse tranformSoapFault (SoapFault soapFault) {
 
     try {
@@ -115,59 +61,66 @@ public class OpaErrorResponseTransformation {
      *     </typ:event>
      *   </typ:events>
      */
-      //if ( OPA_EVENT_ERROR.equalsIgnoreCase(detail.getErrorResponse().getCode())){
-      ObjectFactory factory = new ObjectFactory();
-      AssessResponse assessResponse = factory.createAssessResponse();
+    AssessResponse assessResponse = null;
+    ObjectFactory factory = new ObjectFactory();
+    if ( (detail.getErrorResponse() != null)
+        && (detail.getErrorResponse().getEvents() != null)
+        && (detail.getErrorResponse().getEvents().getEventList() != null) ){
+      assessResponse = factory.createAssessResponse();
       ListEvents listEvents = factory.createListEvents();
-      listEvents.getEvent().add(createEvent(detail, factory));
+      List<RulebaseEvent> rulebaseEvents = listEvents.getEvent();
+      List<Event> events = detail.getErrorResponse().getEvents().getEventList();
+      for ( Event event : events ){
+        rulebaseEvents.add(createEvent(event, factory));
+      }
       assessResponse.setEvents(listEvents);
-      return assessResponse;
-    //}
+    }
+
+    return assessResponse;
   }
 
-  private RulebaseEvent createEvent(Detail detail, ObjectFactory factory) {
-    RulebaseEvent event = null;
-    for ( Event opa12Event : detail.getErrorResponse().getEvents().getEventList()){
-      event = factory.createRulebaseEvent();
-      if ( "error".equalsIgnoreCase(opa12Event.getName()) ){
-        event.setName("Fatal");
-      }
-      event.setEntityId(opa12Event.getInstanceId());
-      event.setMessage(opa12Event.getMessage());
+  private RulebaseEvent createEvent(Event opa12Event, ObjectFactory factory) {
+    RulebaseEvent rulebaseEvent = factory.createRulebaseEvent();
 
-      //Parameters
-      RulebaseEvent.Parameters parameters = factory.createRulebaseEventParameters();
-      parameters.getValue().add(opa12Event.getParameters().getValue());
-      event.setParameters(parameters);
-
-      //Decision Report
-      DecisionReportType decisionReportType = opa12Event.getDecisionReport();
-      if ( decisionReportType != null ){
-        DecisionReport decisionReport = factory.createDecisionReport();
-        decisionReport.setReportStyle(decisionReportType.getReportStyle());
-        addNodes(decisionReport, decisionReportType, factory);
-
-        event.setDecisionReport(decisionReport);
-      }
-
+    if ( "error".equalsIgnoreCase(opa12Event.getName()) ){
+      rulebaseEvent.setName("Fatal");
     }
-    return event;
+    //rulebaseEvent.setEntityId(opa12Event.getInstanceId());
+    rulebaseEvent.setMessage(opa12Event.getMessage());
+
+    //Parameters
+    RulebaseEvent.Parameters parameters = factory.createRulebaseEventParameters();
+    parameters.getValue().add(opa12Event.getParameters().getValue());
+    rulebaseEvent.setParameters(parameters);
+
+    //Decision Report
+    DecisionReportType decisionReportType = opa12Event.getDecisionReport();
+    if ( decisionReportType != null ){
+      DecisionReport decisionReport = factory.createDecisionReport();
+      decisionReport.setReportStyle(decisionReportType.getReportStyle());
+      addNodes(decisionReport, decisionReportType, factory, rulebaseEvent);
+
+      rulebaseEvent.setDecisionReport(decisionReport);
+    }
+
+    return rulebaseEvent;
   }
 
   private void addNodes(DecisionReport opa10DecisionReport,
-              DecisionReportType opa18DecisionReportType, ObjectFactory factory) {
+      DecisionReportType opa18DecisionReportType, ObjectFactory factory,
+      RulebaseEvent rulebaseEvent) {
     for ( Object object : opa18DecisionReportType.getRelationshipNodeOrAttributeNodeOrAlreadyProvenNode() ){
       if ( object instanceof AttributeNodeType ){
         AttributeNodeType opa12NodeType = ( AttributeNodeType ) object;
         AttributeDecisionNode node = factory.createAttributeDecisionNode();
-        createAttributeDecisionNode(node, opa12NodeType, factory);
+        createAttributeDecisionNode(node, opa12NodeType, factory, rulebaseEvent);
         opa10DecisionReport.getRelationshipDecisionNodeOrAttributeDecisionNode().add(node);
       }
     }
   }
 
   private void createAttributeDecisionNode(AttributeDecisionNode node,
-                                  AttributeNodeType opa12NodeType, ObjectFactory factory) {
+      AttributeNodeType opa12NodeType, ObjectFactory factory, RulebaseEvent rulebaseEvent) {
     node.setId(opa12NodeType.getId());
     node.setAttributeId(opa12NodeType.getAttributeId());
     node.setEntityId(opa12NodeType.getInstanceId());
@@ -175,6 +128,8 @@ public class OpaErrorResponseTransformation {
     node.setEntityType(opa12NodeType.getEntityId());
 
     setAttributeTypeEnum( node, opa12NodeType );
+
+    rulebaseEvent.setEntityId(opa12NodeType.getInstanceId());
 
     //Datatype & data
     node.setBooleanVal(opa12NodeType.isBooleanVal());
@@ -215,37 +170,6 @@ public class OpaErrorResponseTransformation {
     Detail detail = (Detail) unmarshaller.unmarshal(soapFault.getDetail());
     logger.debug("*************detail************* : " + detail);
     return detail;
-  }
-
-  /*
-  public static void main(String args[])  {
-    try {
-      File file = new File("/Users/muralidharputtanna/Documents/SoapFault.xml");
-      JAXBContext jaxbContext = JAXBContext.newInstance(Detail.class);
-      Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-      Detail detail = (Detail) unmarshaller.unmarshal(file);
-      System.out.println(detail);
-    } catch (JAXBException e) {
-      e.printStackTrace();
-    }
-  }*/
-
-  private Iterable<Node> iterable(final NodeList nodeList) {
-    return () -> new Iterator<Node>() {
-      private int index = 0;
-
-      @Override
-      public boolean hasNext() {
-        return index < nodeList.getLength();
-      }
-
-      @Override
-      public Node next() {
-        if (!hasNext())
-          throw new NoSuchElementException();
-        return nodeList.item(index++);
-      }
-    };
   }
 
 }
