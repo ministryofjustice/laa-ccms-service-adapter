e4532c1b5dadb96798ea5c98974280933d9a7400
farrell-m <michael.farrell@digital.justice.gov.uk>
2025-05-27 18:01:12 +0100

    dependency upgrades

--- src/main/java/uk/gov/justice/laa/ccms/soap/error/Event.java
+++ src/main/java/uk/gov/justice/laa/ccms/soap/error/Event.java
@@ -2,10 +2,10 @@ package uk.gov.justice.laa.ccms.soap.error;
 
 import com.oracle.determinations.server._12_2_1.rulebase.assess.types.AttributeNodeType;
 import com.oracle.determinations.server._12_2_1.rulebase.assess.types.DecisionReportType;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlAttribute;
+import jakarta.xml.bind.annotation.XmlElement;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Event {
