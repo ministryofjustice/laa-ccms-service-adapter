7a956f35535ce58f4bdaa3bf68e47d7c3a14ac2d
Arun Kumar Madesh <56151567+arunkumar461@users.noreply.github.com>
2025-07-16 12:26:46 +0100

    Migrate springboot 3

--- src/main/java/uk/gov/justice/laa/ccms/AssessAdapterConfig.java
+++ src/main/java/uk/gov/justice/laa/ccms/AssessAdapterConfig.java
@@ -2,7 +2,6 @@ package uk.gov.justice.laa.ccms;
 
 import java.util.Map;
 import java.util.stream.Collectors;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,7 +16,6 @@ public class AssessAdapterConfig {
   @Value("${ccms.ref-data-file}")
   private String opaEntityFileName;
 
-  @Autowired
   public AssessAdapterConfig(ReferenceDataService referenceDataService) {
     this.referenceDataService = referenceDataService;
   }
